f0ac5752898272d10e5ef9edc73e84cc3fc21123
goatn4b1-embera <sonhn@embera.vn>
2026-06-01 09:25:16 +0700

    Update AdminSeeder.php

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -46,7 +46,7 @@ class AdminSeeder extends Seeder
             'floating_back_to_top' => '1',
             'dmca_image' => 'https://cokhimoitruong.com.vn/storage/setting/dmca_1727943209.webp',
             'footer_copyright' => '© Copyright 2008 - 2024 TECOTECH.',
-            'home_meta_keywords' => 'xử lý nước thải, xử lý khí thải, xử lý nước cấp, tổng thầu EPC môi trường, gia công cơ khí, cơ khí môi trường, TECOTECH',
+            'home_meta_keywords' => 'tecotech, TECOTECH, xử lý nước thải, xử lý khí thải, xử lý nước cấp, tổng thầu EPC môi trường, gia công cơ khí, cơ khí môi trường',
             'home_meta_robots' => 'index, follow',
             'site_google_map' => '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3725.2144866632426!2d105.82362037596851!3d20.98404098065494!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ac5df05b5f2f%3A0xb3a5043a4e98f480!2zTMO0IEIsIEvEkFQgxJDhuqFpIEtpbSAtIMSQ4buLbmggQ8O0bmcsIMSQ4buLbmggQ8O0bmcsIEhvw6BuZyBNYWksIEjDoCBO4buZaSwgVmlldG5hbQ!5e0!3m2!1sen!2s!4v1715500000000!5m2!1sen!2s" width="100%" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
         ];
